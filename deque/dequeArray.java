4d2c52a15fd0dd9ca2500916d1b1dc0df826f8cd
JOSE VILANIR DE SOUZA BRITO NETO <20221014040006@ifrn.local>
2024-11-19 15:57:46 -0300

    atividade ponto extra

---
+++ deque/dequeArray.java
@@ -0,0 +1,91 @@
+package deque;
+import java.util.ArrayList;
+
+import javax.print.DocFlavor.INPUT_STREAM;
+
+public class dequeArray {
+    private ArrayList<Object> data;
+    private int size; //quantidade de elementos
+    private int capacity; // tamanho do array
+
+    public dequeArray(){
+        this.capacity = 10;
+        this.size = 0;
+        this.data = new ArrayList<>(capacity);
+    }
+
+
+    public void inserirInicio(Object o){ 
+        if (data.isEmpty()){
+            data.add(o);
+            size++;
+        } 
+        for (int i=size;i > 0; i--){
+            data.add(i,data.get(i - 1));
+        }
+        data.add(0,o);
+        size ++;
+    }
+
+    public Object removerInicio(){
+        if (estaVazio()){
+            throw new IndexOutOfBoundsException("Ta vazia");
+        }
+        Object temp = data.get(0);
+        for (int i=0;i < size; i++){
+            data.add(i,data.get(i + 1));
+        }
+        size --;
+        return temp;
+    }
+
+    
+    public void inserirFim(Object o){
+
+        data.add(o);
+        size++;
+    }
+
+
+    public Object removerFim(){
+
+        if (estaVazio()){
+            throw new IndexOutOfBoundsException("Ta vazia");
+        }
+
+        Object temp = data.get(size - 1);
+        data.remove(size - 1);
+        size --;
+        return temp;
+
+    }
+
+    public Object primeiro(){
+        if (estaVazio()){
+            throw new IndexOutOfBoundsException("Ta vazia");
+        }
+        
+        return data.get(0);
+    }
+
+    public Object ultimo(){
+        if (estaVazio()){
+            throw new IndexOutOfBoundsException("Ta vazia");
+        }
+        
+        return data.get(size - 1);
+    }
+
+    public int tamanho(){
+    return size;
+    }   
+    
+    public boolean estaVazio(){
+        if (size == 0){
+            return true;
+        } else {return false;}
+    }
+}
+
+
+

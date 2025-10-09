3e297c1fd368808c2d2b4daecfe57b841ac10972
josevilanir <104863215+josevilanir@users.noreply.github.com>
2025-10-09 15:02:10 +0000

    Implementação inicial de Rubro Negra AAAAAAAAAAAA

---
+++ EDNL/RBMain.java
@@ -0,0 +1,22 @@
+public class RBMain {
+    public static void main(String[] args) {
+        RedBlackTree<Integer> rbt = new RedBlackTree<>();
+
+        int[] vals = {10, 20, 30, 15, 25, 5, 1, 50, 60, 55, 65, 70};
+        for (int v : vals) rbt.insert(v);
+
+        System.out.println("Depois das inserções:");
+        rbt.printTree();
+
+        int[] rem = {20, 60, 10, 55, 1};
+        for (int k : rem) {
+            System.out.println("Removendo " + k + "...");
+            rbt.remove(k);
+            rbt.printTree();
+        }
+        rbt.insert(55);
+        System.out.println(" Insereindo 55");
+        rbt.printTree();
+
+    }
+}
